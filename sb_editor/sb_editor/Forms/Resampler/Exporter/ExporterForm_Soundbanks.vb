4225e9e4054bc8b90fbb08df35c1801524bbc4b4
jmarti856 <jmarti856@gmail.com>
2022-03-10 23:09:50 +0100

    Build temporal file

--- sb_editor/sb_editor/Forms/Resampler/Exporter/ExporterForm_Soundbanks.vb
+++ sb_editor/sb_editor/Forms/Resampler/Exporter/ExporterForm_Soundbanks.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 Imports sb_editor.ExporterObjects
 Imports sb_editor.ParsersObjects
+Imports sb_editor.SoundBanksExporterFunctions
 
 Partial Public Class ExporterForm
     Public Sub OutputSoundbanks(hashCodesList As SortedDictionary(Of String, UInteger), soundbanksList As String(), streamsList As String(), outLanguages As String(), outPlatforms As String())
@@ -25,6 +26,8 @@ Partial Public Class ExporterForm
                     Dim currentLanguage As String = outLanguages(languageIndex)
                     'For each Platform
                     For platformIndex As Integer = 0 To outPlatforms.Length - 1
+                        Dim CancelSoundBankOutput As Boolean = False
+
                         'Get current platform and update title bar
                         Dim currentPlatform As String = outPlatforms(platformIndex)
                         Invoke(Sub() Text = "Outputting " & currentLanguage & " SoundBank " & currentSoundBank & " for " & currentPlatform)
@@ -39,7 +42,7 @@ Partial Public Class ExporterForm
                         Dim SamplesDictionary As New Dictionary(Of String, EXAudio)
                         Dim SfxList As String() = GetSFXsList(soundBankInfo)
                         GetSFXsDictionary(SfxList, currentPlatform, SfxDictionary, samplesToInclude, streamsList)
-                        GetSamplesDictionary(samplesToInclude, SamplesDictionary, currentPlatform, currentLanguage)
+                        GetSamplesDictionary(samplesToInclude, SamplesDictionary, currentPlatform, currentLanguage, CancelSoundBankOutput)
                         'Skip if there are samples missing
                         If Not CancelSoundBankOutput Then
                             'Get file paths
@@ -100,99 +103,5 @@ Partial Public Class ExporterForm
         Next
     End Sub
 
-    Private Sub WriteSfxFile(binWriter As BinaryWriter, hashCodesList As SortedDictionary(Of String, UInteger), sfxDictionary As SortedDictionary(Of String, EXSound), samplesDictionary As Dictionary(Of String, EXAudio), streamsList As String(), isBigEndian As Boolean)
-        binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(sfxDictionary.Count, isBigEndian))
-        Dim sfxStartOffsets As New Queue(Of UInteger)
-        Dim samplesList As String() = samplesDictionary.Keys.ToArray
-        'SFX header
-        For Each sfxToWrite As EXSound In sfxDictionary.Values
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(sfxToWrite.HashCode, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(0, isBigEndian))
-        Next
-        'SFX parameter entry 
-        Dim StreamFileRefCheckSum As Integer = 0
-        For Each sfxToWrite As EXSound In sfxDictionary.Values
-            sfxStartOffsets.Enqueue(binWriter.BaseStream.Position)
-            binWriter.Write(ESUtils.BytesFunctions.FlipShort(sfxToWrite.DuckerLength, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipShort(sfxToWrite.MinDelay, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipShort(sfxToWrite.MaxDelay, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipShort(sfxToWrite.InnerRadius, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipShort(sfxToWrite.OuterRadius, isBigEndian))
-            binWriter.Write(sfxToWrite.ReverbSend)
-            binWriter.Write(sfxToWrite.TrackingType)
-            binWriter.Write(sfxToWrite.MaxVoices)
-            binWriter.Write(sfxToWrite.Priority)
-            binWriter.Write(sfxToWrite.Ducker)
-            binWriter.Write(sfxToWrite.MasterVolume)
-            binWriter.Write(sfxToWrite.Flags)
-            binWriter.Write(ESUtils.BytesFunctions.FlipUShort(sfxToWrite.Samples.Count, isBigEndian))
-            For sampleIndex As Integer = 0 To sfxToWrite.Samples.Count - 1
-                Dim currentSample As EXSample = sfxToWrite.Samples(sampleIndex)
-                'Find File Ref
-                Dim fileRef As Short
-                If sfxToWrite.HasSubSfx Then
-                    fileRef = hashCodesList(GetOnlyFileName(currentSample.FilePath.TrimStart("\"c)))
-                Else
-                    Dim streamFileIndex As Integer = Array.IndexOf(streamsList, currentSample.FilePath)
-                    If streamFileIndex = -1 Then
-                        fileRef = Array.IndexOf(samplesList, currentSample.FilePath)
-                    Else
-                        fileRef = (streamFileIndex + 1) * -1
-                        PrintLine(1, fileRef & "    " & currentSample.FilePath)
-                        StreamFileRefCheckSum += Math.Abs(fileRef)
-                    End If
-                End If
-                'Write values
-                binWriter.Write(ESUtils.BytesFunctions.FlipShort(fileRef, isBigEndian))
-                binWriter.Write(ESUtils.BytesFunctions.FlipShort(currentSample.PitchOffset, isBigEndian))
-                binWriter.Write(ESUtils.BytesFunctions.FlipShort(currentSample.RandomPitchOffset, isBigEndian))
-                binWriter.Write(currentSample.BaseVolume)
-                binWriter.Write(currentSample.RandomVolumeOffset)
-                binWriter.Write(currentSample.Pan)
-                binWriter.Write(currentSample.RandomPan)
-                binWriter.Write(CByte(0))
-                binWriter.Write(CByte(0))
-            Next
-        Next
-        'Close debug file
-        PrintLine(1, "StreamFileRefCheckSum = " & (StreamFileRefCheckSum * -1))
-        FileClose(1)
-        'Write start offsets
-        binWriter.BaseStream.Seek(8, SeekOrigin.Begin)
-        For index As Integer = 0 To sfxStartOffsets.Count - 1
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(sfxStartOffsets.Dequeue, isBigEndian))
-            binWriter.BaseStream.Seek(4, SeekOrigin.Current)
-        Next
-    End Sub
 
-    Private Sub WriteSifFile(binWriter As BinaryWriter, SamplesDictionary As Dictionary(Of String, EXAudio), isBigEndian As Boolean)
-        binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(SamplesDictionary.Count, isBigEndian))
-        Dim dspHeaderIndex As Integer = 0
-        For Each soundToWrite As EXAudio In SamplesDictionary.Values
-            binWriter.Write(ESUtils.BytesFunctions.FlipInt32(soundToWrite.Flags, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.Address, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.SampleData.Length, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.Frequency, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.RealSize, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.NumberOfChannels, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.Bits, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(dspHeaderIndex * 96, isBigEndian))
-            binWriter.Write(ESUtils.BytesFunctions.FlipUInt32(soundToWrite.LoopOffset, isBigEndian))
-            binWriter.Write(soundToWrite.Duration)
-            dspHeaderIndex += 1
-        Next
-    End Sub
-
-    Private Sub WriteSsfFile(binWriter As BinaryWriter, SamplesDictionary As Dictionary(Of String, EXAudio))
-        For Each soundToWrite As EXAudio In SamplesDictionary.Values
-            binWriter.Write(soundToWrite.DspHeaderData)
-        Next
-    End Sub
-
-    Private Sub WriteSbfFile(binWriter As BinaryWriter, SamplesDictionary As Dictionary(Of String, EXAudio))
-        For Each soundToWrite As EXAudio In SamplesDictionary.Values
-            soundToWrite.Address = binWriter.BaseStream.Position
-            binWriter.Write(soundToWrite.SampleData)
-        Next
-    End Sub
 End Class
--- sb_editor/sb_editor/Forms/Sfx Editor/Frm_SfxEditor_TestSoundbank.vb
+++ sb_editor/sb_editor/Forms/Sfx Editor/Frm_SfxEditor_TestSoundbank.vb
@@ -1,4 +1,7 @@
-﻿Imports sb_editor.ParsersObjects
+﻿Imports System.IO
+Imports sb_editor.ExporterObjects
+Imports sb_editor.ParsersObjects
+Imports sb_editor.SoundBanksExporterFunctions
 
 Partial Public Class Frm_SfxEditor
     Private Sub CreateTestSFX(sfxTextFilePath As String)
@@ -12,5 +15,48 @@ Partial Public Class Frm_SfxEditor
         Dim sfxFilePath As String = fso.BuildPath(outputFolder, &HFFFE & ".sfx")
         Dim sifFilePath As String = fso.BuildPath(outputFolder, &HFFFE & ".sif")
         Dim sbfFilePath As String = fso.BuildPath(outputFolder, &HFFFE & ".sbf")
+
+        'Get SFX and samples list
+        Dim samplesToInclude As New HashSet(Of String)
+        Dim SfxDictionary As New SortedDictionary(Of String, EXSound)
+        Dim SamplesDictionary As New Dictionary(Of String, EXAudio)
+        GetSFXsDictionary(New String() {"Misc\" & fso.GetFileName(sfxTextFilePath)}, "PC", SfxDictionary, samplesToInclude, New String() {}, True)
+        GetSamplesDictionary(samplesToInclude, SamplesDictionary, "PC", "English", False, True)
+
+        'Get output file paths
+        Dim sfxFileName As String = "HC" & Hex(GetSfxFileName(Array.IndexOf(SfxLanguages, "English"), &HFFFE)).PadLeft(6, "0"c)
+        Dim outputFilePath As String = fso.BuildPath(ProjectSettingsFile.MiscProps.EngineXFolder, "Binary\" & GetEngineXFolder("PC") & "\" & GetEngineXLangFolder("English"))
+        CreateFolderIfRequired(outputFilePath)
+
+        'Debug file
+        Dim debugFile As String = fso.BuildPath(WorkingDirectory, "Debug_Report")
+        CreateFolderIfRequired(debugFile)
+        FileOpen(1, fso.BuildPath(debugFile, "StreamDebugSoundBank____SB_TEST_SFX____PC_English.txt"), OpenMode.Output, OpenAccess.Write, OpenShare.LockWrite)
+        PrintLine(1, "SoundBank Output Debug Data")
+        PrintLine(1, Format(Now, "dd/mm/yyy"))
+        PrintLine(1, Format(Now, "hh:mm:ss"))
+        PrintLine(1, "")
+        PrintLine(1, "SoundBankName = ___SB_TEST_SFX___")
+        PrintLine(1, "SoundBankSaveName = 65534")
+        PrintLine(1, "SoundBankFileName = " & sfxFilePath)
+        PrintLine(1, "Stream PoolFiles(n).FileRef")
+
+        'Start Writting file
+        Using sbfWriter As New BinaryWriter(File.Open(sbfFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+            Using sifWriter As New BinaryWriter(File.Open(sifFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                Using sfxWriter As New BinaryWriter(File.Open(sfxFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                    WriteSbfFile(sbfWriter, SamplesDictionary)
+                    WriteSifFile(sifWriter, SamplesDictionary, False)
+                    WriteSfxFile(sfxWriter, Nothing, SfxDictionary, SamplesDictionary, New String() {}, False)
+                End Using
+            End Using
+        End Using
+
+        'Merge files
+        Dim fileOutputPath As String = fso.BuildPath(outputFilePath, sfxFileName & ".SFX")
+        ESUtils.MusXBuild_Soundbank.BuildSoundbankFile(sfxFilePath, sifFilePath, sbfFilePath, Nothing, fileOutputPath, &HFFFE, False)
+
+        'Copy Soundbank to destination folder
+        File.Copy(fileOutputPath, fso.BuildPath(Application.StartupPath, "SystemFiles\testAudioMod\_Eng\" & sfxFileName & ".SFX"), True)
     End Sub
 End Class
