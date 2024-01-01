2a353c806202aa85f29568ab6d9e7608cd840e9d
jmarti856 <jmarti856@gmail.com>
2024-01-01 23:47:45 +0100

    minor changes

--- Solutions/PCAudioDLL/MusX Objects/MusicBank.cs
+++ Solutions/PCAudioDLL/MusX Objects/MusicBank.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace PCAudioDLL.MusX_Objects
+﻿namespace PCAudioDLL.MusX_Objects
 {
     //-------------------------------------------------------------------------------------------------------------------------------
     //-------------------------------------------------------------------------------------------------------------------------------
