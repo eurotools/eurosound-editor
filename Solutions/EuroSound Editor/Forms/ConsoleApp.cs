ed2b26159f26aa7b5d6b4b7cac5758afc66da28d
jmarti856 <jmarti856@gmail.com>
2024-01-01 21:01:09 +0100

    Update ConsoleApp.cs

--- Solutions/EuroSound Editor/Forms/ConsoleApp.cs
+++ Solutions/EuroSound Editor/Forms/ConsoleApp.cs
@@ -126,7 +126,7 @@ namespace sb_editor.Forms
         //-------------------------------------------------------------------------------------------------------------------------------
         private void BtnStart3dSound_Click(object sender, EventArgs e)
         {
-            pcDll.StartSound3D((uint)nudHashCode.Value, new float[] { (float)nudX.Value, (float)nudY.Value, (float)nudZ.Value }, false, chxTestPan.Checked, trckBarMasterVolume.Value);
+            pcDll.StartSound3D((uint)nudHashCode.Value, new float[] { (float)nudX.Value, (float)nudY.Value, (float)nudZ.Value }, 0, chxTestPan.Checked, trckBarMasterVolume.Value);
         }
 
         //-------------------------------------------------------------------------------------------------------------------------------
